7d23099bcc04984afbf3ba321637ba8dbfdbf212
Jan Buchar <Teyras@gmail.com>
2015-03-05 15:33:25 +0100

    added GamePanel

---
+++ src/cz/bucharjan/FlamingAlpacas/GamePanel.java
@@ -0,0 +1,13 @@
+package cz.bucharjan.FlamingAlpacas;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.Color;
+import java.awt.Dimension;
+
+public class GamePanel extends javax.swing.JPanel {
+    public GamePanel () {
+        setBackground(Color.white);
+        setPreferredSize(new Dimension(500, 300));
+    }
+}
--- src/cz/bucharjan/FlamingAlpacas/MainWindow.java
+++ src/cz/bucharjan/FlamingAlpacas/MainWindow.java
@@ -2,15 +2,19 @@ package cz.bucharjan.FlamingAlpacas;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.BorderLayout;
 
 /**
  * Created by teyras on 11.2.15.
  */
 public class MainWindow {
     ServerInterface serverIface;
+    GamePanel panel;
 
     public MainWindow (ServerInterface serverInterface) {
         serverIface = serverInterface;
+        panel = new GamePanel();
+
         serverIface.addUpdateListener((StatusUpdate update) -> {
             System.out.printf("Status update %d received%n", update.getNumber());
         });
@@ -21,6 +25,9 @@ public class MainWindow {
         final JFrame frame = new JFrame("Flaming Alpacas");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         Container pane = frame.getContentPane();
+        pane.setLayout(new BorderLayout());
+
+        pane.add(panel);
 
         frame.pack();
         frame.setVisible(true);
